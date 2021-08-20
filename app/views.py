6a82db65d194ea87800645953625e61ccc1b2a92
isaachintosh <isaac.tuning@gmail.com>
2021-08-20 02:13:21 -0300

    model | migration | routes has been create

--- app/views.py
+++ app/views.py
@@ -1,6 +1,25 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import redirect, render
+from app.forms import CarrosForm
+from app.models import Carros
 
 # Create your views here.
 def home(request):
-    return HttpResponse('Hello World!')
+    data = {}
+    data['db'] = Carros.objects.all()
+    return render(request, 'index.html', data)
+
+def form(request):
+    data = {}
+    data['form'] = CarrosForm()
+    return render(request, 'form.html', data)
+
+def create(request):
+    form = CarrosForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        return redirect('home')
+
+def view(request, pk):
+    data = {}
+    data['db'] = Carros.objects.get(pk=pk)
+    return render(request, 'view.html', data)
